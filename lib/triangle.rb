d558b566c5d9cfce340aa01b61cad3fb86765a4d
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-22 15:15:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
   end
 
   def kind
-    triangle_eval = [(a + b > c), (a + c > b), (b + c > a)]
-    # [a, b, c].each {|value| value <= 0 && a + b > c }
+    triangle_eval = [(a + b > c), (a + c > b), (b + c > a), (a <= 0), (b <= 0), (c <= 0)]
     raise TriangleError if triangle_eval.include?(false)
     if a == b && b == c
       :equilateral
